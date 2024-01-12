f67286b5f4c72242942d02233f601c1657302339
Vitor Rosar Martins <78770551+vitorrosar02@users.noreply.github.com>
2024-01-12 09:07:42 -0300

    Update 1_visao_empresa.py

--- pages/1_visao_empresa.py
+++ pages/1_visao_empresa.py
@@ -167,7 +167,7 @@ df1 = clean_code(df)
 
 st.header("Marketplace - Visão Cliente")
 
-image = Image.open('../logo.jpg')
+image = Image.open('logo.jpg')
 st.sidebar.image(image=image, width=120)
 
 st.sidebar.markdown('# Cury Company')
